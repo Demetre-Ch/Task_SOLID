e0828aa678f00410eeb681e452e83aa34858f408
Demetre Chaligava <demetre_chaligava@epam.com>
2024-09-19 22:06:10 +0400

    'SOLID_Rules_ShoppingCart.cs'

--- SOLIDHomework/SOLIDHomework.Core/ShoppingCart.cs
+++ SOLIDHomework/SOLIDHomework.Core/ShoppingCart.cs
@@ -6,16 +6,80 @@ using System.Threading.Tasks;
 
 namespace SOLIDHomework.Core
 {
+    //Taxes Parent
+    public interface ITaxCalculator
+    {
+        decimal CalculateTax(decimal total);
+    }
+
+    //Maybe Make ICalculator, With 2 Methods CalculateTax and CalculateDiscount if it can be considered as one Task
+
+
+    //Discounts Parent
+    public interface IDiscountCalculator
+    {
+        decimal CalculateDiscount(OrderItem orderItem);
+    }
+
+    public class GlobalTaxCalculator():ITaxCalculator
+    {
+        public decimal CalculateTax(decimal total)
+        {
+            return total * 1.1M;
+        }
+    }
+    
+    public class USTaxCalculator():ITaxCalculator
+    {
+        public decimal CalculateTax(decimal total)
+        {
+            return total * 1.2M;
+        }
+    }
+
+    public class UnitDiscountCalculator():IDiscountCalculator
+    {
+        public decimal CalculateDiscount(OrderItem orderItem)
+        {
+            decimal unitDiscount = 0;
+            if (orderItem.SeassonEndDate <= DateTime.Now)
+            {
+                unitDiscount = 20;
+            }
+            return orderItem.Amount * orderItem.Price * (1 - unitDiscount / 100m);
+        }
+    }
+
+    public class SpecialDiscountCalculator():IDiscountCalculator
+    {
+        public decimal CalculateDiscount(OrderItem orderItem)
+        {
+            decimal total = orderItem.Amount * orderItem.Price;
+            int setsOfFour = orderItem.Amount / 4;
+            return total - setsOfFour * orderItem.Price;
+        }
+
+    }
+
     //there are OCP and SOC violation
     //
     public class ShoppingCart
     {
+
         private readonly string country;
         private readonly List<OrderItem> orderItems;
+        private readonly USTaxCalculator usTaxCalculator;
+        private readonly GlobalTaxCalculator globalTaxCalculator;
+        private readonly UnitDiscountCalculator unitDiscountCalculator;
+        private readonly SpecialDiscountCalculator specialDiscountCalculator;
 
-        public ShoppingCart(string country)
+        public ShoppingCart(string country, USTaxCalculator uSTaxCalculator, GlobalTaxCalculator globalTaxCalculator, UnitDiscountCalculator unitDiscountCalculator, SpecialDiscountCalculator specialDiscountCalculator)
         {
             this.country = country;
+            this.usTaxCalculator = uSTaxCalculator;
+            this.globalTaxCalculator = globalTaxCalculator;
+            this.unitDiscountCalculator = unitDiscountCalculator;
+            this.specialDiscountCalculator = specialDiscountCalculator;
             orderItems = new List<OrderItem>();
         }
 
@@ -23,10 +87,14 @@ namespace SOLIDHomework.Core
         {
             get { return orderItems; }
         }
+
+
         public void Add(OrderItem orderItem)
         {
             orderItems.Add(orderItem);
         }
+
+
         public decimal TotalAmount()
         {
             decimal total = 0;
@@ -35,31 +103,24 @@ namespace SOLIDHomework.Core
             {
                 if (orderItem.Type == "Unit")
                 {
-                    decimal unitDiscount = 0;
-                    //appply 20& discount for old seasons
-                    if (orderItem.SeassonEndDate <= DateTime.Now)
-                    {
-                        unitDiscount = 20;
-                    }
-                    total = orderItem.Amount * orderItem.Price * (1 - unitDiscount / 100m);
+                   total = unitDiscountCalculator.CalculateDiscount(orderItem);
                 }
                     //when buy 4 prodcuts - get one for free!
                 else if (orderItem.Type == "Special")
                 {
-                    total += orderItem.Amount * orderItem.Price;
-                    int setsOfFour = orderItem.Amount / 4;
-                    total -= setsOfFour * orderItem.Price; //discount on groups of 4 items
+                    total = specialDiscountCalculator.CalculateDiscount(orderItem);
                 }
 
             }
             if(country == "US")
             {
-                total *=1.2M;
+                total = usTaxCalculator.CalculateTax(total)
             }
             else
             {
-                total *= 1.1M;
+                total = globalTaxCalculator.CalculateTax(total);
             }
+
             return total;
         }
 
